2a470ad55b887162ae6db16a1662a2a92849b45d
stefano.occhi <stefano.occhi@24consulting.it>
2022-10-04 14:43:56 +0200

    add vv-badge and reset volver style scss

---
+++ src/components/VvBadge/VvBadge.vue
@@ -0,0 +1,18 @@
+<template>
+	<span class="vv-badge" :class="hasClass" role="status">{{ value }}</span>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+	value: string | number
+	color: string
+}>()
+
+const hasClass = computed(() => (props.color ? `vv-badge--${props.color}` : ''))
+</script>
+
+<style lang="scss">
+@import '@volverjs/style/components/vv-badge';
+</style>
