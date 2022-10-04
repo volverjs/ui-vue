b7b49f1f5e7465aad2d462c59d14ae7e7c459782
stefano.occhi <stefano.occhi@24consulting.it>
2022-10-04 17:52:58 +0200

    add icon component and iconify package

---
+++ src/components/VvIcon/VvIcon.vue
@@ -0,0 +1,15 @@
+<template>
+	<Icon :icon="name" />
+</template>
+
+<script lang="ts">
+import { Icon } from '@iconify/vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	components: { Icon },
+	props: {
+		name: String
+	}
+})
+</script>
