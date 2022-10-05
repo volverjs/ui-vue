91fd60bf06730a6678fb1d890d26751d4a01d4b5
stefano.occhi <stefano.occhi@24consulting.it>
2022-10-05 15:15:02 +0200

    handle storybook theme changes to toggle theme-dark class

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,18 @@
 import '../src/stories/stories.scss'
+import addons from '@storybook/addons'
+
+// get an instance to the communication channel for the manager and preview
+const channel = addons.getChannel()
+// switch body class for story
+channel.on('updateGlobals', ({ globals }) => {
+	// Intercept storybook global background changes to toggle css class "theme-dark"
+	const isDark = globals?.backgrounds?.value === '#333333'
+	if (isDark) {
+		document.querySelector('html').classList.add('theme-dark')
+	} else {
+		document.querySelector('html').classList.remove('theme-dark')
+	}
+})
 
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
