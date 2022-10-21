c3254b8e1fec696472fffe9decf44802ad7bbd45
stefano.occhi <stefano.occhi@24consulting.it>
2022-10-21 18:34:59 +0200

    start vv-accordion

---
+++ src/components/VvAccordion/VvAccordion.vue
@@ -0,0 +1,33 @@
+<template>
+	<details role="group" :class="hasClass">
+		<summary class="vv-collapse__header">{{ title }}</summary>
+		<p class="vv-collapse__content">
+			{{ content }}
+		</p>
+	</details>
+</template>
+
+<script setup lang="ts">
+import { computed, type ComputedRef } from 'vue'
+import { useModifiers } from '../../composables/useModifiers'
+
+export interface VvAccordionProps {
+	title: string
+	content: string
+	iconRight?: boolean
+	modifiers?: string | string[]
+}
+
+const props = defineProps<VvAccordionProps>()
+
+const hasModifiers: ComputedRef<string> = useModifiers(
+	'vv-collapse',
+	props.modifiers
+)
+
+const hasClass = computed(() => ['vv-collapse', hasModifiers.value])
+</script>
+
+<style lang="scss">
+@import '@volverjs/style/scss/components/vv-collapse';
+</style>
