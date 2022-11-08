6a8a96833302d0d097ca17d42dc73b8abadbc8ee
stefano.occhi <stefano.occhi@24consulting.it>
2022-11-08 12:11:58 +0100

    remove old useModifiers and update icon

--- src/composables/useModifiers.ts
+++ src/composables/useModifiers.ts
@@ -1,27 +1,5 @@
-import { computed, unref, type ComputedRef, type Ref } from 'vue'
+import { computed, unref, type Ref } from 'vue'
 import ObjectUtilities from '@/utils/ObjectUtilities'
-
-/**
- * @description Create css modifiers classes
- * @param prefix - Prefix for css classes (e.g. 'vv-text', 'vv-icon', etc)
- * @param modifiers - String modifier or Array of string modifiers
- * @returns {ComputedRef<string>} - Reactive css modifiers classes
- */
-export function useModifiers(
-	prefix = '',
-	modifiers: string[] | string = []
-): ComputedRef<string> {
-	return computed(() => {
-		if (typeof modifiers === 'string') {
-			return `${prefix}--${modifiers}`
-		} else if (Array.isArray(modifiers)) {
-			return modifiers
-				.map((modifier) => `${prefix}--${modifier}`)
-				.join(' ')
-		}
-		return ''
-	})
-}
 
 interface IBemModifiers {
 	[key: string]:
