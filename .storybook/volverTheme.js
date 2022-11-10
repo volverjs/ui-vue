cdeac46929f3265b8c5680f7982e9b86b46e8c86
stefano.occhi <stefano.occhi@24consulting.it>
2022-11-10 10:24:24 +0100

    add volver beta logo

---
+++ .storybook/volverTheme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming'
+
+export default create({
+	brandTitle: '@volverjs/ui-vue',
+	brandUrl: 'https://github.com/volverjs/ui-vue',
+	brandImage: 'volverjs-beta.png', //'https://place-hold.it/350x150',
+	brandTarget: '_blank'
+})
