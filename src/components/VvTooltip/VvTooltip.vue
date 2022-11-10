723f340d4ac12b6339fecb99a9d353378e184e26
stefano.occhi <stefano.occhi@24consulting.it>
2022-11-10 12:19:33 +0100

    add tooltip

---
+++ src/components/VvTooltip/VvTooltip.vue
@@ -0,0 +1,10 @@
+<template>
+	<span class="vv-tooltip"
+		>Hover me
+		<span inert role="tooltip" class="vv-tooltip__content"
+			>I'm a tooltip</span
+		>
+	</span>
+</template>
+
+<script setup lang="ts"></script>
