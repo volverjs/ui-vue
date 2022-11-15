8881e555c9f6c18abe991ba4a5f7ae12271dbce2
Michele Belloni <michele.belloni@MacBook-Pro-di-Michele-Belloni.local>
2022-11-15 17:35:55 +0100

    test radio group options

---
+++ src/stories/RadioGroup/RadioOption.test.ts
@@ -0,0 +1,29 @@
+import type { PlayAttributes } from '@/test/types'
+import { within } from '@storybook/testing-library'
+import { expect } from '@/test/expect'
+
+type optionFn = (a: { [key: string]: string }) => string
+
+function getValueToTest(
+	option: { [key: string]: string },
+	key: string | optionFn
+) {
+	return typeof key == 'string' ? `${option[key]}` : `${key(option)}`
+}
+
+async function test({ canvasElement, ...data }: PlayAttributes) {
+	const radioGroup = await within(canvasElement).findByRole('radiogroup')
+	const radioGroupWrapper = radioGroup.children[0]
+	const optionParamLabel = data.args.optionLabel || 'label'
+	const optionParamValue = data.args.optionValue || 'value'
+	const options: { [key: string]: string }[] = data.args.options
+	options.forEach((option, index: number) => {
+		const radio = radioGroupWrapper.children[index] as HTMLElement
+		const radioValue = radio.children[0].value
+		const radioLabel = radio.innerText
+		expect(radioLabel).toEqual(getValueToTest(option, optionParamLabel))
+		expect(radioValue).toEqual(getValueToTest(option, optionParamValue))
+	})
+}
+
+export { test }
