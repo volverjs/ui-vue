0a8c2ee25f4ae7b502e124185a46eeb15ac978cc
stefano.occhi <stefano.occhi@24consulting.it>
2023-01-20 15:06:27 +0100

    add storybook version addon

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -7,6 +7,7 @@ module.exports = {
 		'@storybook/addon-essentials',
 		'@storybook/addon-interactions',
 		'storybook-dark-mode',
+		'storybook-version'
 	],
 	framework: {
 		name: '@storybook/vue3-vite',
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,23 @@
 import { setup } from '@storybook/vue3'
 import VolverPlugin from '@/Volver'
+import packageJson from '../package.json'
 import iconsSimple from '@/assets/icons/simple.json'
 import iconsNormal from '@/assets/icons/normal.json'
 import iconsDetailed from '@/assets/icons/detailed.json'
 import { themes } from '@storybook/theming'
 import './style.scss'
+
+const fullVersion = packageJson.version
+const fullSplittedVersion = fullVersion.split('-')
+let postfix = null 
+if(fullSplittedVersion.length > 1) {
+	// ex: 'beta.1'
+	postfix = fullSplittedVersion[1]
+}
+const splittedVersion = fullSplittedVersion[0].split('.')
+const major = splittedVersion[0] || 0
+const minor = splittedVersion[1] || 0
+const patch = splittedVersion[2] || 0
 
 setup((app) => {
 	const bodyClasses = document.getElementsByTagName('body')[0].classList
@@ -33,6 +46,12 @@ export const parameters = {
 	},
 	docs: {
 		theme: themes.normal,
+	},
+	version: {
+		major,
+		minor,
+		patch,
+		postfix
 	},
 	darkMode: {
 		classTarget: 'body',
