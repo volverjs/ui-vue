e83a42627f2045d6c6a9e01bf8499babfbc025c7
stefano.occhi <stefano.occhi@24consulting.it>
2023-03-31 14:41:08 +0200

    test update, add nav item title and nav separator

--- src/stories/Nav/Nav.test.ts
+++ src/stories/Nav/Nav.test.ts
@@ -1,6 +1,6 @@
 import type { PlayAttributes } from '@/test/types'
 import { expect } from '@/test/expect'
-import { within } from '@storybook/testing-library'
+import { within, userEvent } from '@storybook/testing-library'
 
 export async function defaultTest({ canvasElement, args }: PlayAttributes) {
 	const element = (await within(canvasElement).findByTestId(
@@ -13,6 +13,19 @@ export async function defaultTest({ canvasElement, args }: PlayAttributes) {
 
 	// check children number the same of items prop
 	expect(element.children?.[0].children.length).toEqual(args.items?.length)
+
+	// take firse and second elements
+	const firstEl = await element.getElementsByTagName('a')?.[0]
+	const secondEl = await element.getElementsByTagName('router-link')?.[1]
+	// check they have not current class
+	const secondElHasCurrentClass = secondEl.classList.contains('current')
+	const firstElHasCurrentClass = firstEl.classList.contains('current')
+
+	await expect(firstElHasCurrentClass).toBe(false)
+	await expect(secondElHasCurrentClass).toBe(false)
+	// click first item and check "current" css class
+	await userEvent.click(firstEl)
+	await expect(firstEl).toHaveClass('current')
 
 	// check accessibility
 	await expect(element).toHaveNoViolations()
