2682ca7d0becfc0707eafbc86fc7d75109d40c53
matteo.ginetti <>
2023-05-04 17:04:06 +0200

    Edit Checkbox

---
+++ src/stories/Checkbox/CheckboxBinary.stories.ts
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import VvCheckbox from '@/components/VvCheckbox/VvCheckbox.vue'
+import { defaultArgs, argTypes } from './Checkbox.settings'
+import { Default } from './Checkbox.stories'
+
+const meta: Meta<typeof VvCheckbox> = {
+	title: 'Components/Checkbox/Binary',
+	component: VvCheckbox,
+	args: defaultArgs,
+	argTypes,
+	tags: ['autodocs'],
+}
+
+export default meta
+
+type Story = StoryObj<typeof VvCheckbox>
+
+export const TrueFalse: Story = {
+	...Default,
+	args: {
+		...defaultArgs,
+		value: true,
+		uncheckedValue: false,
+	},
+}
+
+export const YesNo: Story = {
+	...Default,
+	args: {
+		...defaultArgs,
+		value: 'Yes',
+		uncheckedValue: 'No',
+	},
+}
---
+++ src/stories/Checkbox/CheckboxSlots.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import VvCheckbox from '@/components/VvCheckbox/VvCheckbox.vue'
+import { defaultArgs, argTypes } from './Checkbox.settings'
+import { Default } from './Checkbox.stories'
+
+const meta: Meta<typeof VvCheckbox> = {
+	title: 'Components/Checkbox/Slots',
+	component: VvCheckbox,
+	args: defaultArgs,
+	argTypes,
+	tags: ['autodocs'],
+}
+
+export default meta
+
+type Story = StoryObj<typeof VvCheckbox>
+
+export const DefaultSlot: Story = {
+	...Default,
+	args: {
+		...defaultArgs,
+		default: 'Default <em class="italic">slot!</em>',
+	},
+}
+
+export const HintSlot: Story = {
+	...Default,
+	args: {
+		...defaultArgs,
+		hint: 'Hint <em class="italic">slot!</em>',
+	},
+}
