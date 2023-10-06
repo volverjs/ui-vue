59b17f049a933856f796b52c4364bda85c3c4494
Alessandro Bellesia <alessandro.bellesia@24consulting.it>
2023-10-06 10:41:38 +0200

    Fix v-tooltip directive
    fix: `v-tooltip` directive update on props change
    feat: inner types export

--- src/directives/v-tooltip.ts
+++ src/directives/v-tooltip.ts
@@ -1,15 +1,24 @@
 import { render, type Directive, type DirectiveBinding } from 'vue'
 import VvTooltip from '../components/VvTooltip/VvTooltip.vue'
-import type { Side } from '../constants'
+import { Side } from '../constants'
 
-const tooltip: Directive = {
-	beforeMount(el: HTMLElement, binding: DirectiveBinding) {
-		const tooltip = h(VvTooltip, {
-			value: binding.value,
-			position: binding.arg as Side,
-		})
-		render(tooltip, el)
-	},
-}
+const tooltip: Directive = (() => {
+	return {
+		beforeMount(el: HTMLElement, binding: DirectiveBinding) {
+			const tooltip = h(VvTooltip, {
+				value: binding.value,
+				position: binding.arg as Side,
+			})
+			render(tooltip, el)
+		},
+		updated(el: HTMLElement, binding: DirectiveBinding) {
+			const tooltip = h(VvTooltip, {
+				value: binding.value,
+				position: binding.arg as Side,
+			})
+			render(tooltip, el)
+		},
+	}
+})()
 
 export default tooltip
