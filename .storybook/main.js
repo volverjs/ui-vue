30868fbcf4d1e9636978c65bd46365fbb5e8c532
Alessandro Bellesia <alessandro.bellesia@24consulting.it>
2024-04-09 20:52:43 +0200

    Fix type definition
    fix: type definition

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,4 @@
-import type { StorybookConfig } from '@storybook/vue3-vite'
-
-const config: StorybookConfig = {
+const config = {
 	stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
 	addons: [
 		'@storybook/addon-links',
